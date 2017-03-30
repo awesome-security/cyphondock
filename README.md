e2edba534fb930e3eeb50942e1254ed4b738b025
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-03-29 23:46:22 -0400

    add files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cyphondock
+
+A collection of [Docker Compose](https://docs.docker.com/compose/) files
+and configurations for running [Cyphon](https://github.com/dunbarcyber/cyphon)
+and [Cyclops](https://github.com/dunbarcyber/cyclops).
+
+See Cyphon's documentation for a [Docker install](http://cyphon.readthedocs.io/en/latest/getting-started/getting-started.html) for more information.
---
+++ config-COPYME/cyphon/settings/dev.py
@@ -0,0 +1,81 @@
+"""
+[`source`_]
+
+Development Django settings for Cyphon.
+
+For more information on this Django file, see:
+https://docs.djangoproject.com/en/1.9/topics/settings/
+
+For the full list of Django settings and their values, see:
+https://docs.djangoproject.com/en/1.9/ref/settings/
+
+.. _source: ../_modules/cyphon/settings/dev.html
+
+"""
+
+# standard library
+import os
+import logging
+
+# local
+from .base import *
+
+LOGGER = logging.getLogger(__name__)
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost']
+
+#: URL for constructing link with MEDIA_URL
+BASE_URL = 'http://localhost:8000'
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = ['http://localhost:8000']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(name)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'WARNING',
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+    },
+    'loggers': {
+        'django.server': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG'
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG'
+        },
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'receiver': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
