ac1e8988e427e571b0047a205fee358bc0baaea7
lhadjchikh <leila.hadj-chikh@dunbarsecured.com>
2017-04-03 19:05:06 -0400

    delete commented out code

--- config-COPYME/cyphon/settings/prod.py
+++ config-COPYME/cyphon/settings/prod.py
@@ -34,7 +34,6 @@ CORS_ALLOW_CREDENTIALS = True
 #: URL for constructing link with MEDIA_URL, e.g. https://www.example.com
 BASE_URL = 'http://localhost:8000'
 
-# LOG_DIR = '/var/log/cyphon'
 LOG_DIR = BASE_DIR
 
 LOGGING = {
